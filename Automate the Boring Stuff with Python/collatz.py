9fa1cb4e3e266a15ac733819a57ad43c6c605836
nthatf01 <nthatfield@gmail.com>
2017-04-17 21:44:00 -0400

    Fixed errors with user input

--- Automate the Boring Stuff with Python/collatz.py
+++ Automate the Boring Stuff with Python/collatz.py
@@ -1,6 +1,10 @@
 def collatz(number):
     while (number != 1):
+        if number % 2 == 0:
+            number = number / 2
+        else:
+            number = 3 * number + 1
         print(number)
-        number = 1
-
-collatz(input())
+        
+userInput = int(input())
+collatz(userInput)
