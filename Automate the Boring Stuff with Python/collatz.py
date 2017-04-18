70d29d26b408b26ddc73bc9c5a28bb9231534b2e
nthatf01 <nthatfield@gmail.com>
2017-04-17 21:52:29 -0400

    Added error handling to validate input

--- Automate the Boring Stuff with Python/collatz.py
+++ Automate the Boring Stuff with Python/collatz.py
@@ -1,10 +1,24 @@
+#Demonstrates the Collatz Sequence
+
+#Define the collatz function
 def collatz(number):
     while (number != 1):
+        
+        #If the value is even, divide by 2
         if number % 2 == 0:
             number = number / 2
+            
+        #Else, if the value is odd, multiple by 3 and add 1
         else:
             number = 3 * number + 1
+            
         print(number)
-        
-userInput = int(input())
-collatz(userInput)
+
+#Collect user input
+userInput = input()
+
+#Error handling to validate input as an integer, call the collatz function
+try:
+    collatz(int(userInput))
+except ValueError:
+    print('Error: You must input an integer')
