bc060cab4455d762e44556a9bcf1975fc55fb082
nthatf01 <nthatfield@gmail.com>
2017-04-23 17:16:51 -0400

    Finished project runs as specified

--- Automate the Boring Stuff with Python/commaCode.py
+++ Automate the Boring Stuff with Python/commaCode.py
@@ -1,6 +1,9 @@
 def listToString(listParameter):
-    for i in listParameter:
-        print(i + ', ')
-
+    for i in range(len(listParameter)):
+        if (i != len(listParameter) - 1):
+            print(listParameter[i] + ', ', end = '')
+        else:
+            print('and ' + listParameter[i])
+        
 spam = ['apples', 'bananas', 'tofu', 'cats']
 listToString(spam)
